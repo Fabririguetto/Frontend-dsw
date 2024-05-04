fc19fafd7af56e8757b88a05034f67f1e030400d
fabri <Fabririguetto@gmail.com>
2024-05-04 12:43:58 -0300

    creacion del front con react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          no hicimos nada todavia
+        </p>
+        <img src='https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDkwNzV2cDdtbzM3MnFrczJqb2V5bjJmcnRiZm9oOG15OWx0Z2cxZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RLW9YEaSBfBMt79fm4/giphy.gif'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
