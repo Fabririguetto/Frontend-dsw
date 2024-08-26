bb363d1106047f04189a283eb2b839c2653f7a31
fabri <Fabririguetto@gmail.com>
2024-08-26 20:40:48 -0300

    creation: clientes / Fix:stock

--- src/forms/clientes.js
+++ src/forms/clientes.js
@@ -3,6 +3,12 @@ import '../App.css';
 
 function FormClientes() {
   const [clientes, setClientes] = useState([]);
+  const [dni, setDni] = useState('');
+  const [nombreApellido, setNombreApellido] = useState('');
+  const [direccion, setDireccion] = useState('');
+  const [contacto, setContacto] = useState('');
+  const [idCliente, setIdCliente] = useState(''); 
+  const [sortConfig, setSortConfig] = useState({ key: 'idCliente', direction: 'ascending' }); // Estado para ordenar
 
   useEffect(() => {
     fetchClientes();
@@ -15,31 +21,159 @@ function FormClientes() {
         if (Array.isArray(clientes)) {
           setClientes(clientes);
         } else {
-          setClientes([]); // En caso de que la respuesta no sea un array
+          setClientes([]); 
         }
       })
       .catch((error) => {
         console.error('Error fetching clientes:', error);
-        setClientes([]); // En caso de error, establecer clientes como un array vacío
+        setClientes([]);
       });
   };
+
+  const handleSearchClientes = async (nombre) => {
+    let url = 'http://localhost:3500/clientes';
+
+    if (nombre) {
+        url += `?nombre=${encodeURIComponent(nombre)}`;
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.statusText}`);
+        }
+
+        const clientes = await response.json();
+        console.log('Clientes encontrados:', clientes);
+
+        if (Array.isArray(clientes)) {
+            setClientes(clientes);
+        } else {
+            setClientes([]);
+        }
+    } catch (error) {
+        console.error('Error al buscar clientes:', error);
+        setClientes([]);
+    }
+};
+
+  const handleIngresar = (event) => {
+    event.preventDefault();
+
+    const cliente = {
+      dni,
+      nombre_apellidoCli: nombreApellido,
+      direccion,
+      contacto,
+    };
+
+    if (idCliente) {
+      updateCliente(idCliente, cliente);
+    } else {
+      createCliente(cliente);
+    }
+  };
+
+  const createCliente = (cliente) => {
+    fetch('http://localhost:3500/clientes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cliente),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Cliente ingresado correctamente:', data);
+        fetchClientes();
+        resetForm();
+      })
+      .catch((error) => {
+        console.error('Error al ingresar el cliente:', error);
+      });
+  };
+
+  const updateCliente = (id, cliente) => {
+    fetch(`http://localhost:3500/clientes/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cliente),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Cliente modificado correctamente:', data);
+        fetchClientes();
+        resetForm();
+      })
+      .catch((error) => {
+        console.error('Error al modificar el cliente:', error);
+      });
+  };
+
+  const handleEdit = (cliente) => {
+    setIdCliente(cliente.idCliente);
+    setDni(cliente.dni);
+    setNombreApellido(cliente.nombre_apellidoCli);
+    setDireccion(cliente.direccion);
+    setContacto(cliente.contacto);
+  };
+
+
+  const resetForm = () => {
+    setIdCliente('');
+    setDni('');
+    setNombreApellido('');
+    setDireccion('');
+    setContacto('');
+  };
+
+  // Función para manejar la solicitud de ordenamiento
+  const requestSort = (key) => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
+
+  // Función para ordenar los clientes basados en la configuración de ordenamiento
+  const sortedClientes = [...clientes].sort((a, b) => {
+    if (a[sortConfig.key] < b[sortConfig.key]) {
+      return sortConfig.direction === 'ascending' ? -1 : 1;
+    }
+    if (a[sortConfig.key] > b[sortConfig.key]) {
+      return sortConfig.direction === 'ascending' ? 1 : -1;
+    }
+    return 0;
+  });
 
   const renderClientes = () => {
     if (clientes.length === 0) {
       return (
         <tr>
-          <td colSpan="5">No hay clientes disponibles</td>
+          <td colSpan="6">No hay clientes disponibles</td>
         </tr>
       );
     }
 
-    return clientes.map((cliente) => (
+    return sortedClientes.map((cliente) => (
       <tr key={cliente.idCliente}>
         <td>{cliente.idCliente}</td>
         <td>{cliente.dni}</td>
         <td>{cliente.nombre_apellidoCli}</td>
         <td>{cliente.direccion}</td>
         <td>{cliente.contacto}</td>
+        <td>
+          <button id='botmod' onClick={() => handleEdit(cliente)}>Modificar</button>
+        </td>
       </tr>
     ));
   };
@@ -47,23 +181,70 @@ function FormClientes() {
   return (
     <div className="App">
       <header className="App-header">
-        <input type="text" id="filtro" placeholder="Buscar..." />
+        <input
+          type="text"
+          id="filtro"
+          placeholder="Buscar clientes por nombre o DNI..."
+          onChange={(e) => handleSearchClientes(e.target.value)}
+        />
       </header>
+      <form onSubmit={handleIngresar}>
+        <input
+          type="text"
+          id="idCliente"
+          placeholder="ID Cliente"
+          value={idCliente}
+          readOnly
+        />
+        <input
+          type="text"
+          id="dni"
+          placeholder="DNI"
+          value={dni}
+          onChange={(e) => setDni(e.target.value)}
+        />
+        <input
+          type="text"
+          id="nombreApellido"
+          placeholder="Nombre y Apellido"
+          value={nombreApellido}
+          onChange={(e) => setNombreApellido(e.target.value)}
+        />
+        <input
+          type="text"
+          id="direccion"
+          placeholder="Dirección"
+          value={direccion}
+          onChange={(e) => setDireccion(e.target.value)}
+        />
+        <input
+          type="text"
+          id="contacto"
+          placeholder="Contacto"
+          value={contacto}
+          onChange={(e) => setContacto(e.target.value)}
+        />
+        <button type="submit" id="ingresarCliente">
+          {idCliente ? 'Modificar' : 'Ingresar'}
+        </button>
+      </form>
       <div className="tabla-container">
-        <table id="tabla-clientes" className="tabla-negra">
+        {<table id="tabla-clientes" className="tabla-negra">
           <thead>
             <tr>
-              <th className="columna">ID Cliente</th>
-              <th className="columna">DNI</th>
-              <th className="columna">Nombre y Apellido</th>
-              <th className="columna">Dirección</th>
-              <th className="columna">Contacto</th>
+              <th className="columna" onClick={() => requestSort('idCliente')}>ID Cliente</th>
+              <th className="columna" onClick={() => requestSort('dni')}>DNI</th>
+              <th className="columna" onClick={() => requestSort('nombre_apellidoCli')}>Nombre y Apellido</th>
+              <th className="columna" onClick={() => requestSort('direccion')}>Dirección</th>
+              <th className="columna" onClick={() => requestSort('contacto')}>Contacto</th>
+              <th className="columna">Acciones</th>
             </tr>
           </thead>
           <tbody className="cuerpo-tabla">
             {renderClientes()}
           </tbody>
         </table>
+        }
       </div>
     </div>
   );
--- src/forms/stock.js
+++ src/forms/stock.js
@@ -8,111 +8,132 @@ function Formstock() {
   const [descripcion, setDescripcion] = useState('');
   const [cantidad, setCantidad] = useState('');
   const [monto, setMonto] = useState('');
-  const [idProducto, setIdProducto] = useState(''); // Nuevo estado para idProducto
+  const [idProducto, setIdProducto] = useState(''); 
+  const [sortConfig, setSortConfig] = useState({ key: 'idProducto', direction: 'ascending' });
+  const [estado, setEstado] = useState('Alta');
+  const [nombreProducto, setNombreProducto] = useState('');
+
 
   useEffect(() => {
     fetchProductos();
   }, []);
 
   const fetchProductos = () => {
-    fetch('http://localhost:3500/productos')
+    fetch('http://localhost:3500/stock')
       .then((response) => response.json())
       .then((productos) => {
         if (Array.isArray(productos)) {
           setProductos(productos);
         } else {
-          setProductos([]); // En caso de que la respuesta no sea un array
+          setProductos([]); 
         }
       })
       .catch((error) => {
         console.error('Error fetching productos:', error);
-        setProductos([]); // En caso de error, establecer productos como un array vacío
+        setProductos([]);
       });
   };
 
-  const handleSubmit = (event) => {
+  const handleingresar = (event) => {
     event.preventDefault();
 
     const producto = {
-      articulo: articulo,
-      descripcion: descripcion,
-      cantidad: cantidad,
-      monto: monto,
+      articulo,
+      descripcion,
+      cantidad,
+      monto,
     };
 
     if (idProducto) {
-      // Modificar producto
-      fetch(`http://localhost:3500/productos/${idProducto}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(producto),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log('Producto modificado correctamente:', data);
-          fetchProductos();
-          resetForm();
-        })
-        .catch((error) => {
-          console.error('Error al modificar el producto:', error);
-        });
+      updateProducto(idProducto, producto);
     } else {
-      // Insertar nuevo producto
-      fetch('http://localhost:3500/productos', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(producto),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log('Producto ingresado correctamente:', data);
-          fetchProductos();
-          resetForm();
-        })
-        .catch((error) => {
-          console.error('Error al ingresar el producto:', error);
-        });
+      createProducto(producto);
     }
   };
 
-  const handleSearch = async (event) => {
-    const nombreProducto = event.target.value;
+  const createProducto = (producto) => {
+    fetch('http://localhost:3500/stock', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(producto),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Producto ingresado correctamente:', data);
+        fetchProductos();
+        resetForm();
+      })
+      .catch((error) => {
+        console.error('Error al ingresar el producto:', error);
+      });
+  };
 
-    let url = `http://localhost:3500/productos`;
+  const updateProducto = (id, producto) => {
+    fetch(`http://localhost:3500/stock/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(producto),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Producto modificado correctamente:', data);
+        fetchProductos();
+        resetForm();
+      })
+      .catch((error) => {
+        console.error('Error al modificar el producto:', error);
+      });
+  };
+
+  const handleEstadoChange = (event) => {
+    const newEstado = event.target.value;
+    setEstado(newEstado);
+    handleSearch(newEstado, nombreProducto); // Actualiza el estado y llama a la búsqueda
+};
+  
+const handleProductoChange = (event) => {
+    const newNombreProducto = event.target.value;
+    setNombreProducto(newNombreProducto);
+    handleSearch(estado, newNombreProducto); // Actualiza el nombreProducto y llama a la búsqueda
+};
+  
+const handleSearch = async (estado, nombreProducto) => {
+    let url = `http://localhost:3500/stock?estado=${encodeURIComponent(estado)}`;
+
     if (nombreProducto) {
-      url += `?producto=${encodeURIComponent(nombreProducto)}`;
+        url += `&producto=${encodeURIComponent(nombreProducto)}`;
     }
 
     try {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.statusText}`);
+        }
 
-      const productos = await response.json();
-      console.log('Productos encontrados:', productos);
+        const productos = await response.json();
+        console.log('Productos encontrados:', productos);
 
-      if (Array.isArray(productos)) {
-        setProductos(productos);
-      } else {
-        setProductos([]); // En caso de que la respuesta no sea un array
-      }
-
+        if (Array.isArray(productos)) {
+            setProductos(productos);
+        } else {
+            setProductos([]);
+        }
     } catch (error) {
-      console.error('Error al buscar productos:', error);
-      setProductos([]); // En caso de error, establecer productos como un array vacío
+        console.error('Error al buscar productos:', error);
+        setProductos([]);
     }
-  };
+};
+
 
   const handleEdit = (producto) => {
     setIdProducto(producto.idProducto);
@@ -120,6 +141,30 @@ function Formstock() {
     setDescripcion(producto.descripcion);
     setCantidad(producto.cantidad);
     setMonto(producto.monto);
+  };
+
+  const handleElim = (producto) => {
+    if (window.confirm(`¿Seguro que quieres cambiar el estado del producto ${producto.articulo}?`)) {
+      deleteProducto(producto.idProducto, producto.estado);
+    }
+  };
+  
+  const deleteProducto = (id, estadoActual) => {
+    fetch(`http://localhost:3500/stockelim/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ estado: estadoActual }), 
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Producto actualizado correctamente:', data);
+        fetchProductos();
+      })
+      .catch((error) => {
+        console.error('Error al actualizar el estado del producto:', error);
+      });
   };
 
   const resetForm = () => {
@@ -130,16 +175,36 @@ function Formstock() {
     setMonto('');
   };
 
+  // Función para manejar la solicitud de ordenamiento
+  const requestSort = (key) => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
+
+  // Función para ordenar los productos basados en la configuración de ordenamiento
+  const sortedProductos = [...productos].sort((a, b) => {
+    if (a[sortConfig.key] < b[sortConfig.key]) {
+      return sortConfig.direction === 'ascending' ? -1 : 1;
+    }
+    if (a[sortConfig.key] > b[sortConfig.key]) {
+      return sortConfig.direction === 'ascending' ? 1 : -1;
+    }
+    return 0;
+  });
+
   const renderProductos = () => {
     if (productos.length === 0) {
       return (
         <tr>
-          <td colSpan="6">No hay productos disponibles</td>
+          <td colSpan="7">No hay productos disponibles</td>
         </tr>
       );
     }
 
-    return productos.map((producto) => (
+    return sortedProductos.map((producto) => (
       <tr key={producto.idProducto}>
         <td>{producto.idProducto}</td>
         <td>{producto.articulo}</td>
@@ -147,9 +212,8 @@ function Formstock() {
         <td>{producto.cantidad}</td>
         <td>{producto.monto || 'No disponible'}</td>
         <td>
-          <button height='1px' onClick={() => handleEdit(producto)}>
-            Modificar
-          </button>
+          <button id='botmod' onClick={() => handleEdit(producto)}>Modificar</button>
+          <button id='botelim' onClick={() => handleElim(producto)}>Eliminar</button>
         </td>
       </tr>
     ));
@@ -162,10 +226,14 @@ function Formstock() {
           type="text"
           id="filtro"
           placeholder="Buscar..."
-          onChange={handleSearch}
+          onChange={handleProductoChange}
         />
+        <select id="estado1" onChange={handleEstadoChange} value={estado}>
+          <option value="alta">Alta</option>
+          <option value="baja">Baja</option>
+        </select>
       </header>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleingresar}>
         <input
           type="text"
           id="idProducto"
@@ -206,14 +274,14 @@ function Formstock() {
         </button>
       </form>
       <div className="tabla-container">
-        <table id="tabla-prod" className="tabla-negra">
+        {<table id="tabla-prod" className="tabla-negra">
           <thead>
             <tr>
-              <th className='columna'>ID Producto</th>
-              <th className="columna">Artículo</th>
-              <th className="columna">Descripción</th>
-              <th className="columna">Cantidad</th>
-              <th className="columna">Precio de venta</th>
+              <th className="columna" onClick={() => requestSort('idProducto')}>ID Producto</th>
+              <th className="columna" onClick={() => requestSort('articulo')}>Artículo</th>
+              <th className="columna" onClick={() => requestSort('descripcion')}>Descripción</th>
+              <th className="columna" onClick={() => requestSort('cantidad')}>Cantidad</th>
+              <th className="columna" onClick={() => requestSort('monto')}>Precio de venta</th>
               <th className="columna">Acciones</th>
             </tr>
           </thead>
@@ -221,6 +289,7 @@ function Formstock() {
             {renderProductos()}
           </tbody>
         </table>
+        }
       </div>
     </div>
   );
